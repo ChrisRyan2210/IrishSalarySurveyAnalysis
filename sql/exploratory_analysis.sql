79e48aea99d87267e5414e7ef068367b87c84ab0
Chris Ryan <150379234+ChrisRyan2210@users.noreply.github.com>
2024-07-30 21:10:52 +0100

    Update exploratory_analysis.sql
    moving some sql code to other file

--- sql/exploratory_analysis.sql
+++ sql/exploratory_analysis.sql
@@ -1,17 +1,6 @@
 --Exploratory Analysis
 USE DevilEireSalarySurvey;
 
---Changing column type to int for analysis
-BEGIN TRANSACTION;
-ALTER TABLE [SurveyResults_2024]
-ALTER COLUMN [Total_Comp] FLOAT;
-GO
---Checking for errors
-SELECT *
-FROM SurveyResults_2024;
-GO
-COMMIT;
-
 --Analysis
 SELECT Age_Bracket, ROUND(AVG(Total_Comp), 0) AS Avg_Comp, COUNT(*) AS NumberOfEntries
 FROM SurveyResults_2024
